48ff2e7107d8fdec3bbcc0477fc9c7369b8c1144
Nick Jackson <nick@nickjackson.me>
2018-09-27 13:50:30 +0100

    Admin improvements

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(models.Location, LocationAdmin)
+
+
+class NameInline(admin.TabularInline):
+    model = models.Name
+
+
+class MemorialImageInline(admin.StackedInline):
+    model = models.MemorialImage
+
+
+class MemorialAdmin(admin.ModelAdmin):
+
+    list_display = ('pretty_name', 'names_on_memorial', 'location', 'complete')
+
+    inlines = [
+        NameInline, MemorialImageInline
+    ]
+
+
+admin.site.register(models.Memorial, MemorialAdmin)
+
+
+class NameAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'date_of_birth', 'date_of_death', 'memorial_name', 'memorial_location')
+
+    def memorial_name(self, obj):
+        return obj.memorial.pretty_name()
+    memorial_name.admin_order_field = 'memorial__name'
+
+    def memorial_location(self, obj):
+        return obj.memorial.location.name
+    memorial_location.admin_order_field = 'memorial__location__name'
+
+
+admin.site.register(models.Name, NameAdmin)
+
+
+admin.site.register(models.MemorialImage)
