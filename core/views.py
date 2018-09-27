6a8bd1fb194937bec2bd9ff4b180a91815f8cf24
Nick Jackson <nick@nickjackson.me>
2018-09-27 13:00:51 +0100

    Home page and layout!

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView, ListView, DetailView
+
 # Create your views here.
+
+
+class index(TemplateView):
+
+    template_name = 'index.html'
