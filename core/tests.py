2c8948effbdfa307e7c4336784279145e7cda600
Nick Jackson <nick@dxw.com>
2020-06-22 22:16:37 +0100

    Add basic smoke test for home page

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase, override_settings
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class AcceptanceTests(TestCase):
+    """ Site acceptance tests for core functions """
+
+    def test_homepage_returns(self):
+        """ Make sure the homepage responds with something. """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Memorials at Whitkirk")
